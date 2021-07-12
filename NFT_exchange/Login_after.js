b40a68651bd65cbc661c41dd4947feb598f4482f
hyunji96377 <gus9637@naver.com>
2021-07-12 12:30:37 +0900

    핫한작품 map으로 리스트

--- NFT_exchange/Login_after.js
+++ NFT_exchange/Login_after.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {  Image, TextInput, StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
+import { Image, TextInput, StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -19,17 +19,41 @@ import Qrcode from './component/Qrcode';
 import ETH_BTN from './component/ETH_btn';
 import ThemedListItem from 'react-native-elements/dist/list/ListItem';
 import AsyncStorage from '@react-native-community/async-storage';
+import Slide_profile from './component/Slide_profile';
+import Slide_txt1 from './component/Slide_txt1';
+import Slide_txt2 from './component/Slide_txt2';
 
 const { width } = Dimensions.get("window");
 const height = width * 0.5;
-const images = [
-    'https://img4.yna.co.kr/photo/etc/epa/2019/12/06/PEP20191206054201848_P4.jpg',
-    'https://ichef.bbci.co.uk/news/800/cpsprodpb/C173/production/_117832594_066549055.jpg',
-    'http://res.heraldm.com/content/image/2013/05/27/20130527000159_0.jpg',
-    'http://www.economyf.com/pds_update/umg_20200528234240.jpg',
-    'http://db.kookje.co.kr/news2000/photo/2018/1206/L20181206.22021001594i1.jpg'
 
+const dataList = [
+    {
+        imageUrl: "https://img4.yna.co.kr/photo/etc/epa/2019/12/06/PEP20191206054201848_P4.jpg",
+        title: "title1",
+        content: "content1"
+    },
+    {
+        imageUrl: "https://ichef.bbci.co.uk/news/800/cpsprodpb/C173/production/_117832594_066549055.jpg",
+        title: "title2",
+        content: "content2"
+    },
+    {
+        imageUrl: "http://res.heraldm.com/content/image/2013/05/27/20130527000159_0.jpg",
+        title: "title3",
+        content: "content3"
+    },
+    {
+        imageUrl: "http://www.economyf.com/pds_update/umg_20200528234240.jpg",
+        title: "title4",
+        content: "content4"
+    },
+    {
+        imageUrl: "http://db.kookje.co.kr/news2000/photo/2018/1206/L20181206.22021001594i1.jpg",
+        title: "title5",
+        content: "content5"
+    },
 ]
+
 
 const Input = () => {
 
@@ -68,8 +92,8 @@ const Input = () => {
 
                 </TouchableOpacity>
 
-                <Qrcode 
-                marginTop = {10}/>
+                <Qrcode
+                    marginTop={10} />
             </View>
 
         </>
@@ -82,10 +106,10 @@ export default class MainScreen extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {result: 0}   
+        this.state = { result: 0 }
     }
 
-    async componentDidMount () {
+    async componentDidMount() {
         let a = 80;
 
         AsyncStorage.setItem('ETH', a.toString(), () => {
@@ -94,10 +118,10 @@ export default class MainScreen extends React.Component {
 
         // ETH 초기값 할당
         AsyncStorage.getItem('ETH', (err, result) => {
-            
+
             console.log(result); // User1 출력
-            this.setState({result})
-            
+            this.setState({ result })
+
         });
 
         /* AsyncStorage.getItem('ETH_after', (err, result2) => {
@@ -133,54 +157,88 @@ export default class MainScreen extends React.Component {
 
                 </View>
 
-                <View style={{ marginTop: 30, width, height }}>
+                <View style={{ marginTop: 30, width: 30, height: 550 }}>
                     <ScrollView pagingEnabled horizontal style={{ width, height }}>
 
-                        {images.map((image, index) => (
-                            <Image
-                                key={index}
-                                source={{ uri: image }}
-                                style={{ width, height, resizeMode: 'contain' }} />
+                        {dataList.map((element, index) => (
+                            <View key={index}>
+                                <Image
+                                    key={index}
+                                    source={{ uri: element.imageUrl }}
+                                    style={{ width, height, resizeMode: 'contain' }} />
+
+                                <Slide_profile
+                                    title={element.title}
+                                    onPress={() => this.goArtist_Screen()}
+                                />
+
+
+                                <NFT_name
+                                    title={element.content}
+                                    marginLeft={50}
+                                    fontSize={45}
+                                    onPress={() => this.goNFT_detailScreen()} />
+
+
+                                <View style={{ marginTop: 5, marginLeft: 50 }}>
+
+                                    <Slide_txt1 />
+
+                                    <Slide_txt2 />
+
+
+                                    <Text style={{ marginTop: 5 }}>10.000원</Text>
+
+                                </View>
+
+
+                                <CustomButton
+                                    button_marginLeft={48}
+                                    onPress={() => this.goNFT_detailScreen()} />
+
+                            </View>
+
                         ))
+
+
                         }
+
                     </ScrollView>
                 </View>
 
-                {/*여기 부터가 detail_main입니다.*/}
-                <View style={{ marginTop: 10, marginLeft: 30 }}>
-                    <Profile
-                        title={'hyunji'}
-                        onPress={() => this.goArtist_Screen()} />
-                    <NFT_name
-                        title={'NATURE'}
-                        marginLeft={20}
-                        onPress={() => this.goNFT_detailScreen()} />
-                    <Detail_main />
-                    <CustomButton
-                        onPress={() => this.goNFT_detailScreen()} />
-                </View>
 
                 <View>
                     <Hot_text />
                 </View>
 
                 {/*여기 부터가 Nft_simple_info 프레임입니다.*/}
-                <View style={{ marginTop: 10, marginLeft: 55 }}>
-                    <Nft_simple_info_cardImage
-                        source={{ uri: 'https://cdn.eyesmag.com/content/uploads/posts/2020/09/29/studio-ghibli-releases-400-free-to-use-images-01-0be601c8-2b4d-41f7-ba3c-f3a1a19697a6.jpg' }}
-                        onPress={() => this.goNFT_detailScreen()} />
-                    <View style={styles.cardContainer}>
-                        <NFT_name
-                            title={'The hill'}
-                            fontSize={20}
-                            onPress={() => this.goNFT_detailScreen()} />
-                        <Nft_simple_info_Profile
-                            title={'hyunji'}
-                            onPress={() => this.goArtist_Screen()} />
-                        <Nft_simple_info_costtime
-                            nft_cost={'0.01ETH'} />
+                {dataList.map((element, index) => (
+                    <View key={index}>
+
+                        <View style={{ marginTop: 10, marginLeft: 55 }}>
+                            <Nft_simple_info_cardImage
+                                source={{ uri: element.imageUrl }}
+                                onPress={() => this.goNFT_detailScreen()} />
+                            <View style={styles.cardContainer}>
+                                <NFT_name
+                                    title={element.content}
+                                    fontSize={20}
+                                    onPress={() => this.goNFT_detailScreen()} />
+                                <Nft_simple_info_Profile
+                                    title={element.title}
+                                    onPress={() => this.goArtist_Screen()} />
+                                <Nft_simple_info_costtime
+                                    nft_cost={'0.01ETH'} />
+                            </View>
+                        </View>
+
+
                     </View>
-                </View>
+
+                ))
+
+
+                }
 
             </ScrollView>
         );
