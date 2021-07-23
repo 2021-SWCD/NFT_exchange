04bf5fbe758e34d1f8a4913a66d0517862191e27
minj-L <wonder_minji@naver.com>
2021-07-23 16:04:46 +0900

    nft_simple_info_cost를 nft_cost로 공용화

---
+++ NFT_exchange/component/common/commonElement/Nft_cost.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View, } from 'react-native';
+
+export default class Nft_cost extends Component{
+    static defaultProps = {
+      nft_cost: '0',
+      nft_cost_color: 'black',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 10,
+    }
+    constructor(props){
+        super(props);
+    }
+    render() {
+      return(
+        <View style={[{marginTop: this.props.marginTop}]}>
+            <Text style={[
+              {color: this.props.nft_cost_color},
+              {fontSize: this.props.fontSize},
+              {fontWeight: this.props.fontWeight},
+              ]}>{this.props.nft_cost} ETH</Text>
+        </View>
+      )
+    }
+}
