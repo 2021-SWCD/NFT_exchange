06917a1627d81ed5eda37b5df565bd90836bc463
minj-L <wonder_minji@naver.com>
2021-07-23 17:23:37 +0900

    핫한작품 datalist json파일로 연동

--- NFT_exchange/component/common/nftSimpleInfoCard/NftSimpleInfoCard.js
+++ NFT_exchange/component/common/nftSimpleInfoCard/NftSimpleInfoCard.js
@@ -5,34 +5,9 @@ import NFT_name from '../commonElement/Nft_name';
 import Profile from '../commonElement/Profile';
 import Nft_cost from '../commonElement/Nft_cost';
 import Timer from '../commonElement/Timer';
+import dataList from '..//../../datalist.json'
 
-const dataList = [
-    {
-      imageUrl: "https://img4.yna.co.kr/photo/etc/epa/2019/12/06/PEP20191206054201848_P4.jpg",
-      title: "title1",
-      content: "content1"
-    },
-    {
-      imageUrl: "https://ichef.bbci.co.uk/news/800/cpsprodpb/C173/production/_117832594_066549055.jpg",
-      title: "title2",
-      content: "content2"
-    },
-    {
-      imageUrl: "http://res.heraldm.com/content/image/2013/05/27/20130527000159_0.jpg",
-      title: "title3",
-      content: "content3"
-    },
-    {
-      imageUrl: "http://www.economyf.com/pds_update/umg_20200528234240.jpg",
-      title: "title4",
-      content: "content4"
-    },
-    {
-      imageUrl: "http://db.kookje.co.kr/news2000/photo/2018/1206/L20181206.22021001594i1.jpg",
-      title: "title5",
-      content: "content5"
-    },
-]
+
 
 export default class NftSimpleInfoCard extends React.Component {
   render(){
