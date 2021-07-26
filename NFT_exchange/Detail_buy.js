7a0202ffc9dd2809d9fff67af0ad0de9045b07f3
minj-L <wonder_minji@naver.com>
2021-07-26 15:59:55 +0900

    import 경로 수정

--- NFT_exchange/Detail_buy.js
+++ NFT_exchange/Detail_buy.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Text, StyleSheet, View, ScrollView, } from 'react-native';
 import { LoginHeader, LoginAfterHeader } from './component/common/logIn';
 import Go_main from './component/common/goMain/go_main';
-import Detail_main from './component/detail_main';
-import { CustomButton, Custom_cancel} from './component/common/commonElement';
+import { CustomButton, Custom_cancel, Detail_main} from './component/common/commonElement';
 import { Buy_text, Buy_screen } from './component/buy/buyElement';
 import AsyncStorage from '@react-native-community/async-storage';
 
