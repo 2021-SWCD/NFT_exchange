b63c0fd424a768aef8ff21127ab209286c1acfd2
hyunji96377 <gus9637@naver.com>
2021-07-26 10:17:33 +0900

    wrongsearch페이지 안쓰이는 코드 전부 삭제

--- NFT_exchange/WrongSearch.js
+++ NFT_exchange/WrongSearch.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView,  } from 'react-native';
+import { StyleSheet, View, ScrollView, } from 'react-native';
 import { LoginHeader, LoginAfterHeader } from './component/common/logIn';
-
 import NftSimpleInfoCard from './component/common/nftSimpleInfoCard/NftSimpleInfoCard';
 import Hot_text from './component/common/commonElement/Hot_text';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -35,7 +34,7 @@ export default class WrongSearch extends React.Component {
 
   checkLoginStatus = () => {
     AsyncStorage.getItem('logIncom', (err, result) => {
-      console.log('Login_after'); 
+      console.log('Login_after');
       this.setState({ isLoggedIn: JSON.parse(result) })
     });
   };
@@ -62,19 +61,13 @@ export default class WrongSearch extends React.Component {
           <Text style={styles.wrong_text}>다른 검색어로 검색해주세요.</Text>
         </View> */}
 
-        <Wrong_text/>
+        <Wrong_text />
 
-        <View>
-          <Hot_text />
-        </View>
+        <Hot_text />
 
         {/*NftSimpleInfoCard*/}
-        <View style={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: 20,
-        }}>
-          <NftSimpleInfoCard navigation={this.props.navigation}/>
+        <View style={styles.cardContainer}>
+          <NftSimpleInfoCard navigation={this.props.navigation} />
         </View>
 
       </ScrollView>
@@ -116,69 +109,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'white'
   },
 
-  topView: {
-    flexDirection: 'row',
-    height: 55,
-    backgroundColor: 'white',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  korbiBtn: {
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-  loginBtn: {
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    fontSize: 15,
-    fontWeight: 'normal',
-  },
-
   
-  
-  
-  
-  fire: {
-    color: 'red',
-    marginLeft: 10,
-
-
-  },
-
-  hot_text: {
-    marginLeft: 8,
-    marginRight: 20,
-
-    fontSize: 25,
-    fontWeight: 'bold',
-
-  },
-  scrollView: {
-    flex: 10,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  slide_img: {
-    height: 270,
-    width: 250,
-    marginTop: 25,
-    marginLeft: 10,
-    marginRight: 10,
-
-  },
-
   cardContainer: {
-    backgroundColor: '#d3d3d3',
-    borderBottomLeftRadius: 20, // 모서리 둥글게 테두리를 통틀어서 border라고 하나보다
-    borderBottomRightRadius: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 20,
-    width: 300,
-    height: 170,
   },
 });
