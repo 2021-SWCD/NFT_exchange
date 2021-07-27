7b5cc51cd5303911c7e67e2b02acefe730630f3d
hyunji96377 <gus9637@naver.com>
2021-07-27 11:11:35 +0900

    eth_btn파일 정리

--- NFT_exchange/component/common/logIn/logInElement/ETH_btn.js
+++ NFT_exchange/component/common/logIn/logInElement/ETH_btn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableOpacity, Text, StyleSheet,View } from 'react-native';
 
 export default class ETH_btn extends React.Component {
