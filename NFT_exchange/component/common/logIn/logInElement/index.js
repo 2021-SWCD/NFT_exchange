b49c84a08706ef92b99eb51a6becbb81c51240c2
hyunji96377 <gus9637@naver.com>
2021-07-27 18:13:57 +0900

    loginelement 폴더 속 파일명들 수정

--- NFT_exchange/component/common/logIn/logInElement/index.js
+++ NFT_exchange/component/common/logIn/logInElement/index.js
@@ -1,3 +1,3 @@
-export {default as ETH_btn} from './ETH_btn';
-export {default as Korbit_logo} from './Korbit_logo';
-export {default as Login_btn} from './Login_btn';
+export {default as ETH_btn} from './EthBtn';
+export {default as Korbit_logo} from './KorbitLogo';
+export {default as Login_btn} from './LoginBtn';
