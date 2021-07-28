3aec2f23222038f9995ff83ca433834afe1a9189
minj-L <wonder_minji@naver.com>
2021-07-28 17:45:02 +0900

    ArtistScreen 언어 추가

--- NFT_exchange/src/config/locales/ko.js
+++ NFT_exchange/src/config/locales/ko.js
@@ -1,15 +1,12 @@
 const ko = {
     welcome : '환영합니다',
-<<<<<<< HEAD
     goMain : '처음으로',
     art : '작품',
-=======
     login : '로그인',
     searchProduct : '작품명 검색',
     noResultTxt : '검색 결과를 찾을 수 없어요.',
     findAnotherTxt : '다른 검색어로 검색해주세요',
     hotText : '핫한 작품',
->>>>>>> 8ec8c7372344bbee9b856c91f4f6dea049ff413c
 };
 
 export default ko;
