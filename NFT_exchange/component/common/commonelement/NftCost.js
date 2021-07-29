9ab400b0594d67bb8655bc3549d033a15f9abf53
hyunji96377 <gus9637@naver.com>
2021-07-29 14:26:04 +0900

    hottext,nftinformation,nftname 변수수정

--- NFT_exchange/component/common/commonelement/NftCost.js
+++ NFT_exchange/component/common/commonelement/NftCost.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import { Text, View, } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 
-export default class NftCost extends Component{
-    static defaultProps = {
-      NftCost: '0',
-      NftCost_color: 'black',
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginTop: null,
-    }
-    constructor(props){
-        super(props);
-    }
-    render() {
-      return(
-        <View style={[{marginTop: this.props.marginTop}]}>
-            <Text style={[
-              {color: this.props.NftCost_color},
-              {fontSize: this.props.fontSize},
-              {fontWeight: this.props.fontWeight},
-              ]}>{this.props.NftCost} ETH</Text>
-        </View>
-      )
-    }
+export default class NftCost extends Component {
+  static defaultProps = {
+    NftCost: '0',
+    NftCost_color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={[{marginTop: this.props.marginTop}]}>
+        <Text
+          style={[
+            {color: this.props.NftCost_color},
+            {fontSize: this.props.fontSize},
+            {fontWeight: this.props.fontWeight},
+          ]}>
+          {this.props.NftCost} ETH
+        </Text>
+      </View>
+    );
+  }
 }
--- NFT_exchange/component/common/commonelement/Profile.js
+++ NFT_exchange/component/common/commonelement/Profile.js
@@ -1,45 +1,49 @@
 //Artist_Screen과 이어짐
-import React from 'react'
-import {StyleSheet, View, Text, TouchableOpacity,} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import 'react-native-gesture-handler';
 
 export default class Profile extends React.Component {
-    static defaultProps = {
-      title: 'untitled',
-      titleColor: 'black',
-      fontSize: 20,
-      marginTop: null,
-      marginLeft: null,
-      name_marginLeft :8,
-      name_marginTop: null,
-      name_width : 150,
-      name_height: 30,
-      profile_width: 30,
-      profile_height: 30,
-      profile_marginTop: null,
-      profile_borderRadius: 25,
-      profile_backgroundColor: 'black',
-      onPress: () => null,
-    }
-    constructor(props){
-      super(props);
-    }    
-    render(){
-        return (
-        <TouchableOpacity onPress={() => this.goArtist_Screen()} >
-          <View style={[
+  static defaultProps = {
+    title: 'untitled',
+    titleColor: 'black',
+    fontSize: 20,
+    marginTop: null,
+    marginLeft: null,
+    name_marginLeft: 8,
+    name_marginTop: null,
+    name_width: 150,
+    name_height: 30,
+    profile_width: 30,
+    profile_height: 30,
+    profile_marginTop: null,
+    profile_borderRadius: 25,
+    profile_backgroundColor: 'black',
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.goArtist_Screen()}>
+        <View
+          style={[
             styles.row,
             {marginTop: this.props.marginTop},
             {marginLeft: this.props.marginLeft},
           ]}>
-            <View style={[
+          <View
+            style={[
               {width: this.props.profile_width},
               {height: this.props.profile_height},
               {marginTop: this.props.profile_marginTop},
               {borderRadius: this.props.profile_borderRadius},
               {backgroundColor: this.props.profile_backgroundColor},
-            ]}/>
-            <Text style={[
+            ]}
+          />
+          <Text
+            style={[
               {fontSize: this.props.fontSize},
               {color: this.props.titleColor},
               {marginLeft: this.props.name_marginLeft},
@@ -47,11 +51,11 @@ export default class Profile extends React.Component {
               {width: this.props.name_width},
               {height: this.props.name_height},
             ]}>
-              {this.props.title}
-            </Text>
-          </View>
-        </TouchableOpacity>
-    )
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
   }
   goArtist_Screen() {
     // ARTIST_screen으로 화면 이동
@@ -59,9 +63,11 @@ export default class Profile extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({ //원하는 구성 요소들은 여기서 설정해줘야 한다.
-    row: { //프로필, 이름등을 가지고 있는 가로 정렬을 위한 요소
-      
-      flexDirection: 'row',
-    },
+const styles = StyleSheet.create({
+  //원하는 구성 요소들은 여기서 설정해줘야 한다.
+  row: {
+    //프로필, 이름등을 가지고 있는 가로 정렬을 위한 요소
+
+    flexDirection: 'row',
+  },
 });
