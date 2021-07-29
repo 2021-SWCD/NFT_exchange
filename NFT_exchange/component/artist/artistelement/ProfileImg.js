79a6d7a916bd275301d9f013ac09ee6268ab676b
minj-L <wonder_minji@naver.com>
2021-07-29 11:01:44 +0900

    클래스명 변경후 코드 정리

--- NFT_exchange/component/artist/artistelement/ProfileImg.js
+++ NFT_exchange/component/artist/artistelement/ProfileImg.js
@@ -1,31 +1,27 @@
 //Artist폴더 만든후 아티스트 요소에 넣기
-import React from 'react'
-import {StyleSheet, View,  TouchableOpacity,} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import 'react-native-gesture-handler';
 
-export default class Profile_img extends React.Component {
-      
-    render(){
-        return (
-            <TouchableOpacity onPress={this.props.onPress}>
-                
-                  <View style={styles.user_profile}/>
-                 
-                
-            </TouchableOpacity>
-        )
-    }
+export default class ProfileImg extends React.Component {
+  render() {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View style={styles.user_profile} />
+      </TouchableOpacity>
+    );
+  }
 }
 
-const styles = StyleSheet.create({ //원하는 구성 요소들은 여기서 설정해줘야 한다.
-    
-    user_profile: { //검정색 원의 프로필
-      width: 110,
-      height: 110,
-      borderRadius: 55,
-      backgroundColor: 'black',
-      
-      marginLeft: 110,
-      
-    },
+const styles = StyleSheet.create({
+  //원하는 구성 요소들은 여기서 설정해줘야 한다.
+
+  user_profile: {
+    //검정색 원의 프로필
+    width: 110,
+    height: 110,
+    borderRadius: 55,
+    backgroundColor: 'black',
+    marginLeft: 110,
+  },
 });
--- NFT_exchange/component/artist/artistelement/ProfileName.js
+++ NFT_exchange/component/artist/artistelement/ProfileName.js
@@ -1,37 +1,39 @@
 //Artist_Screen과 이어짐
-import React from 'react'
-import { View, Text, TouchableOpacity,} from 'react-native';
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 import 'react-native-gesture-handler';
 
-export default class Profile_name extends React.Component {
-    static defaultProps = {
-      title: 'untitled',
-      titleColor: 'black',
-      fontSize: 30,
-      fontWeight:'bold',
-      marginLeft:165,
-      marginTop:30,
-      marginBottom:30,
-      onPress: () => null,
-    }
-    constructor(props){
-      super(props);
-    }    
-    render(){
-        return (
-            <TouchableOpacity onPress={this.props.onPress}>
-                <View>                  
-                  <Text style={[
-                    {color: this.props.titleColor},
-                    {fontSize: this.props.fontSize},
-                    {marginTop: this.props.marginTop},
-                    {marginBottom: this.props.marginBottom}, 
-                    {marginLeft: this.props.marginLeft},
-                    {fontWeight: this.props.fontWeight},
-                  ]}>{this.props.title}</Text>
-                </View>
-            </TouchableOpacity>
-        )
-    }
+export default class ProfileName extends React.Component {
+  static defaultProps = {
+    title: 'untitled',
+    titleColor: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginLeft: 165,
+    marginTop: 30,
+    marginBottom: 30,
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View>
+          <Text
+            style={[
+              {color: this.props.titleColor},
+              {fontSize: this.props.fontSize},
+              {marginTop: this.props.marginTop},
+              {marginBottom: this.props.marginBottom},
+              {marginLeft: this.props.marginLeft},
+              {fontWeight: this.props.fontWeight},
+            ]}>
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
-
