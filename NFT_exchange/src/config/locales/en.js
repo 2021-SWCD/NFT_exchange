bad18888357c0e9a2cf4e9ac7e03790237f257c9
hyunji96377 <gus9637@naver.com>
2021-07-29 15:48:49 +0900

    언어 컴포넌트/페이지 주석으로 구분

--- NFT_exchange/src/config/locales/en.js
+++ NFT_exchange/src/config/locales/en.js
@@ -1,37 +1,48 @@
 const en = {
-  goMain: 'GoMain',
-  art: 'Art',
-  login : 'LOGIN',
-  loginBtn: 'LOGIN',
-  noResultTxt: 'There is no result.',
-  findAnotherTxt: 'Please search with another term',
+  //component/buy/buyelement/BuyScreen.js
+  //component/common/commonelement/NftInformation.js
+  won: 'won',
+
+  //component/buy/buyelement/BuyText.js
+  sugPrice: 'SUGGEST PRICE',
+
+  //component/common/commonelement/CustomButton.js
+  suggestBtn: 'SUGGEST',
+
+  //component/common/commonelement/CustomCancelButton.js
+  cancelButton: 'Cancel',
+
+  //component/common/commonelement/HotText.js
   hotText: 'HOT PRODUCT',
+
+  //component/common/commonelement/NftInformation.js
   curAuction: 'Current Auction Price',
   remainTime: 'Time remaining',
-  won: 'won',
-  offering: 'Offering',
-  cancelButton: 'Cancel',
-  explainProductTxt: 'explain product',
-  ownerTxt: 'owner',
-  noSugTxt: 'no one suggest yet.',
-  doSugTxt: 'Offer a price!',
-  loginSafeText:
-    'Please check the address window for safe \nuse of the exchange.',
-  safeIconText: 'Safety',
-  korbitEmailAccount: 'E-mail',
-  passWord: 'Password',
-  logout: 'Logout',
-  notLoginState: 'Login please',
+
+  //component/common/commonelement/QrWallet.js
   notLoginStateContent1: 'This service',
   notLoginStateContent2: 'requires login.',
+
+  //component/common/commonelement/QrWalletNotLogin.js
+  notLoginState: 'Login please',
   notLoginStateGoLogin: 'Go Login',
   depositEthereum1: 'Scan the QR code',
   depositEthereum2: 'below to deposit Ethereum',
+
+  //component/common/gomain/GoMain.js
+  goMain: 'GoMain',
+
+  //component/common/login/loginelement/LoginBtn.js
+  loginBtn: 'LOGIN',
+
+  //component/common/search/Search.js
+  searchBarTxt: 'search',
+
+  //component/nftdetail/TabBar.js
   suggest: 'Suggest',
   inforamation: 'Info',
-  searchBarTxt: 'search',
-  suggestGuideTxt: 'I suggest the price in the above contents.',
-  suggestBtn: 'SUGGEST',
+
+  //component/nftdetail/WarnTxt.js
   sugModalMain: 'SUG/BUY CAUTIONS',
   sugModal1: '•If you propose a price to buy, there will be a network fee.',
   sugModal2:
@@ -39,10 +50,45 @@ const en = {
   sugModal3: '•Purchased works can be found in the purchase of the profile.',
   sugModal4:
     '•If you have any other questions, please contact Covit Customer Service.',
-  modalOkBtn: 'OK',
+
+  //component/wrong/wrongelement/WrongText.js
+  noResultTxt: 'There is no result.',
+  findAnotherTxt: 'Please search with another term',
+
+  //screen/TabInfoScreen.js
+  explainProductTxt: 'explain product',
+  ownerTxt: 'owner',
+
+  //screen/SuggestScreen.js
+  noSugTxt: 'no one suggest yet.',
+  doSugTxt: 'Offer a price!',
+
+  //screen/LoginScreen.js
+  login: 'LOGIN',
+  loginSafeText:
+    'Please check the address window for safe \nuse of the exchange.',
+  safeIconText: 'Safety',
+  korbitEmailAccount: 'E-mail',
+  passWord: 'Password',
+
+  //screen/ArtistScreen.js
   coinAccount: '132-213546-65543(bitcoin account)',
   artistComment: 'Make a great piece of work!',
-  sugPrice: 'SUGGEST PRICE',
+  art: 'Art',
+
+  //screen/LogoutScreen.js
+  logout: 'Logout',
+
+  //screen/DetailBuyScreen.js
+  suggestGuideTxt: 'I suggest the price in the above contents.',
+
+  //screen/NftDetailScreen.js
+  modalOkBtn: 'OK',
+
+
+
+  
+
 };
 
 export default en;
--- NFT_exchange/src/config/locales/ko.js
+++ NFT_exchange/src/config/locales/ko.js
@@ -1,45 +1,118 @@
-const ko = {
-  goMain: '처음으로',
-  art: '작품',
-  login : '로그인',
-  noResultTxt: '검색 결과를 찾을 수 없어요.',
-  findAnotherTxt: '다른 검색어로 검색해주세요',
+const ko = {//component/buy/buyelement/BuyScreen.js
+  //component/common/commonelement/NftInformation.js
+  won: '원',
+
+  //component/buy/buyelement/BuyText.js
+  sugPrice : '제안가격',
+
+  //component/common/commonelement/CustomButton.js
+  suggestBtn: '제안하기',
+
+  //component/common/commonelement/CustomCancelButton.js
+  cancelButton: '취소하기',
+
+  //component/common/commonelement/HotText.js
   hotText: '핫한 작품',
+
+  //component/common/commonelement/NftInformation.js
   curAuction: '현재 경매가',
   remainTime: '남은 시간',
-  won: '원',
-  offering: '가격 제안',
-  offeringContent: '위 내용으로 가격을 제안합니다.',
-  cancelButton: '취소하기',
-  explainProductTxt: '작품 설명',
-  ownerTxt: '소유자',
-  noSugTxt: '아직 제안한 사람이 없어요.',
-  doSugTxt: '첫번째로 가격을 제안해보세요!',
-  loginSafeText: '안전한 거래소 이용을 위해 주소창을 확인해주세요.',
-  safeIconText: '안전함',
-  korbitEmailAccount: '코빗 이메일 계정',
-  passWord: '비밀번호',
-  logout: '로그아웃',
-  notLoginState: '로그인이 필요합니다.',
+
+  //component/common/commonelement/QrWallet.js
   notLoginStateContent1: '로그인이 필요한',
   notLoginStateContent2: '서비스입니다.',
+
+  //component/common/commonelement/QrWalletNotLogin.js
+
+  notLoginState: '로그인이 필요합니다.',
+  notLoginStateGoLogin: '로그인',
   depositEthereum1: '이더리움을 입금하려면',
   depositEthereum2: '아래 QR코드를 스캔하세요',
+
+  //component/common/gomain/GoMain.js
+  goMain: '처음으로',
+
+  //component/common/login/loginelement/LoginBtn.js
+  loginBtn: '로그인',
+
+  //component/common/search/Search.js
+  searchBarTxt: '작품명 검색',
+
+  //component/nftdetail/TabBar.js
   suggest: '제안',
   inforamation: '정보',
-  loginBtn: '로그인',
-  searchBarTxt: '작품명 검색',
-  suggestBtn: '제안하기',
+
+  //component/nftdetail/WarnTxt.js
   sugModalMain: '제안/구매 유의사항',
   sugModal1: '•구매를 하기 위한 가격 제안 시, 네트워크 수수료가 발생합니다.',
   sugModal2:
     '•회원은 구매를 완료한 작품은 환불할 수 없으며 그에 따른 비용 또는 손해에 대하여 코빗은 책임지지 않습니다.',
   sugModal3: '•구매한 작품은 프로필의 구매에서 확인할 수 있습니다.',
   sugModal4: '•기타 궁금하신 사항은 코빗 고객센터로 문의바랍니다.',
-  modalOkBtn: '확인',
+
+  //component/wrong/wrongelement/WrongText.js
+  noResultTxt: '검색 결과를 찾을 수 없어요.',
+  findAnotherTxt: '다른 검색어로 검색해주세요',
+
+  //screen/TabInfoScreen.js
+  explainProductTxt: '작품 설명',
+  ownerTxt: '소유자',
+
+  //screen/SuggestScreen.js
+  noSugTxt: '아직 제안한 사람이 없어요.',
+  doSugTxt: '첫번째로 가격을 제안해보세요!',
+
+  //screen/LoginScreen.js
+  login : '로그인',
+  loginSafeText: '안전한 거래소 이용을 위해 주소창을 확인해주세요.',
+  safeIconText: '안전함',
+  korbitEmailAccount: '코빗 이메일 계정',
+  passWord: '비밀번호',
+
+  //screen/ArtistScreen.js
   coinAccount : '132-213546-65543(비트코인 계좌)',
   artistComment : '오늘도 멋진 작품을!',
-  sugPrice : '제안가격',
+  art: '작품',
+
+  //screen/LogoutScreen.js
+  logout: '로그아웃',
+
+  //screen/DetailBuyScreen.js
+  suggestGuideTxt: '위 가격을 제안합니다.',
+
+  //screen/NftDetailScreen.js
+  modalOkBtn: '확인',
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
 };
 
 export default ko;
