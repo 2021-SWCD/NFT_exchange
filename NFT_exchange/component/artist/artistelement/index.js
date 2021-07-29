6c6ba1667c6b4fc55fa241945e02cc8e4cc012ec
minj-L <wonder_minji@naver.com>
2021-07-29 11:00:42 +0900

    artistelement폴더 클래스명 변경

--- NFT_exchange/component/artist/artistelement/index.js
+++ NFT_exchange/component/artist/artistelement/index.js
@@ -1,3 +1,3 @@
-export {default as Profile_img} from './ProfileImg';
-export {default as Profile_name} from './ProfileName';
-export {default as Profile_text} from './ProfileText';
+export {default as ProfileImg} from './ProfileImg';
+export {default as ProfileName} from './ProfileName';
+export {default as ProfileText} from './ProfileContent';
