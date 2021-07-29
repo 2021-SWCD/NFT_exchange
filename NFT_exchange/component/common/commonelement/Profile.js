09566b3743304dd3db13f41b582a4c043fc1da1b
minj-L <wonder_minji@naver.com>
2021-07-29 14:50:13 +0900

    코드정리, 스타일, 변수명 변경

--- NFT_exchange/component/common/commonelement/Profile.js
+++ NFT_exchange/component/common/commonelement/Profile.js
@@ -1,57 +1,61 @@
 //Artist_Screen과 이어짐
-import React from 'react'
-import {StyleSheet, View, Text, TouchableOpacity,} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import 'react-native-gesture-handler';
 
 export default class Profile extends React.Component {
-    static defaultProps = {
-      title: 'untitled',
-      titleColor: 'black',
-      fontSize: 20,
-      marginTop: null,
-      marginLeft: null,
-      name_marginLeft :8,
-      name_marginTop: null,
-      name_width : 150,
-      name_height: 30,
-      profile_width: 30,
-      profile_height: 30,
-      profile_marginTop: null,
-      profile_borderRadius: 25,
-      profile_backgroundColor: 'black',
-      onPress: () => null,
-    }
-    constructor(props){
-      super(props);
-    }    
-    render(){
-        return (
-        <TouchableOpacity onPress={() => this.goArtist_Screen()} >
-          <View style={[
-            styles.row,
+  static defaultProps = {
+    title: 'untitled',
+    titleColor: 'black',
+    fontSize: 20,
+    marginTop: null,
+    marginLeft: null,
+    nameMarginLeft: 8,
+    nameMarginTop: null,
+    nameWidth: 150,
+    nameHeight: 30,
+    profileWidth: 30,
+    profileHeight: 30,
+    profileMarginTop: null,
+    profileBorderRadius: 25,
+    profileBackgroundColor: 'black',
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.goArtist_Screen()}>
+        <View
+          style={[
+            styles.rowArray,
             {marginTop: this.props.marginTop},
             {marginLeft: this.props.marginLeft},
           ]}>
-            <View style={[
-              {width: this.props.profile_width},
-              {height: this.props.profile_height},
-              {marginTop: this.props.profile_marginTop},
-              {borderRadius: this.props.profile_borderRadius},
-              {backgroundColor: this.props.profile_backgroundColor},
-            ]}/>
-            <Text style={[
+          <View
+            style={[
+              {width: this.props.profileWidth},
+              {height: this.props.profileHeight},
+              {marginTop: this.props.profileMarginTop},
+              {borderRadius: this.props.profileBorderRadius},
+              {backgroundColor: this.props.profileBackgroundColor},
+            ]}
+          />
+          <Text
+            style={[
               {fontSize: this.props.fontSize},
               {color: this.props.titleColor},
-              {marginLeft: this.props.name_marginLeft},
-              {marginTop: this.props.name_marginTop},
-              {width: this.props.name_width},
-              {height: this.props.name_height},
+              {marginLeft: this.props.nameMarginLeft},
+              {marginTop: this.props.nameMarginTop},
+              {width: this.props.nameWidth},
+              {height: this.props.nameHeight},
             ]}>
-              {this.props.title}
-            </Text>
-          </View>
-        </TouchableOpacity>
-    )
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
   }
   goArtist_Screen() {
     // ARTIST_screen으로 화면 이동
@@ -59,9 +63,10 @@ export default class Profile extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({ //원하는 구성 요소들은 여기서 설정해줘야 한다.
-    row: { //프로필, 이름등을 가지고 있는 가로 정렬을 위한 요소
-      
-      flexDirection: 'row',
-    },
+const styles = StyleSheet.create({
+  //원하는 구성 요소들은 여기서 설정해줘야 한다.
+  rowArray: {
+    //프로필, 이름등을 가지고 있는 가로 정렬을 위한 요소
+    flexDirection: 'row',
+  },
 });
