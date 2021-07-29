decc158fe54c255c5885c530b75104cd5225d4bf
hyunji96377 <gus9637@naver.com>
2021-07-29 11:44:39 +0900

    qrcode.js 변수명 정리

--- NFT_exchange/component/common/commonelement/Qrcode.js
+++ NFT_exchange/component/common/commonelement/Qrcode.js
@@ -1,40 +1,42 @@
-import React  from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
 export default class Qrcode extends React.Component {
-    static defaultProps = {
-        color: 'black',
-        marginLeft: 10,
-        marginTop: null,
-        marginBottom: null,
-        onPress: () => null,
-    }
-    constructor(props){
-        super(props);
-    }
-    render() {
-        return (
-            <TouchableOpacity 
-                onPress={this.props.onPress}
-                style={styles.btnContainer}>
-                        <Icon style={[
-                            {color: this.props.color},
-                            {marginLeft: this.props.marginLeft},
-                            {marginTop: this.props.marginTop},
-                            {marginBottom: this.props.marginBottom},
-                        ]}name="qr-code-outline" size={26} />
-            </TouchableOpacity>
-
-        );
-    }
+  static defaultProps = {
+    color: 'black',
+    marginLeft: 10,
+    marginTop: null,
+    marginBottom: null,
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.props.onPress}
+        style={styles.btnContainer}>
+        <Icon
+          style={[
+            {color: this.props.color},
+            {marginLeft: this.props.marginLeft},
+            {marginTop: this.props.marginTop},
+            {marginBottom: this.props.marginBottom},
+          ]}
+          name="qr-code-outline"
+          size={26}
+        />
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    btnContainer : {
-        position : 'absolute', 
-        top : 10, 
-        left : 320
-    },
-})
+  btnContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 320,
+  },
+});
