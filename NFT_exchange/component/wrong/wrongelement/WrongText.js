302f751e68e4bc346bd6c98339c154eed727fa47
hyunji96377 <gus9637@naver.com>
2021-07-29 10:49:08 +0900

    wrong폴더 변수명 정리

--- NFT_exchange/component/wrong/wrongelement/WrongText.js
+++ NFT_exchange/component/wrong/wrongelement/WrongText.js
@@ -1,46 +1,37 @@
-//Artist폴더 만든후 아티스트 요소에 넣기
-import React from 'react'
-import { StyleSheet, View, Text,  } from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 import 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Ionicons';
-import  I18n  from '../../../src/config/i18n'
-
+import I18n from '../../../src/config/i18n';
 
 export default class wrong_text extends React.Component {
-
-    render() {
-        return (
-            
-            <View style={styles.container}>
-                <Icon style={styles.sad} name="sad-outline" size={45} />
-                <Text style={styles.wrong_text}>{I18n.t('noResultTxt')}</Text>
-                <Text style={styles.wrong_text}>{I18n.t('findAnotherTxt')}</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Icon style={styles.sad} name="sad-outline" size={45} />
+        <Text style={styles.wrongText}>{I18n.t('noResultTxt')}</Text>
+        <Text style={styles.wrongText}>{I18n.t('findAnotherTxt')}</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({ //원하는 구성 요소들은 여기서 설정해줘야 한다.
-    container: {
-        marginTop: 90,
-        alignItems: 'center'
-    },
+const styles = StyleSheet.create({
+  //원하는 구성 요소들은 여기서 설정해줘야 한다.
+  container: {
+    marginTop: 90,
+    alignItems: 'center',
+  },
 
-    sad: {
-        color: 'black',
-        marginBottom: 20,
+  sad: {
+    color: 'black',
+    marginBottom: 20,
+  },
 
-    },
-
-
-    wrong_text: {
-        marginLeft: 10,
-        marginBottom: 5,
-        fontSize: 22,
-        fontWeight: 'bold'
-
-    },
+  wrongText: {
+    marginLeft: 10,
+    marginBottom: 5,
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 });
-
-
-
