89092f54308e6490dc2a5b08ad926af3da2f6dd4
hyunji96377 <gus9637@naver.com>
2021-07-29 11:58:29 +0900

    nftsimpleinfocard 폴더 변수 정리

--- NFT_exchange/component/common/commonelement/CardImage.js
+++ NFT_exchange/component/common/commonelement/CardImage.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-export default class CardImage extends Component{
-    static defaultProps = {
-        borderBottomLeftRadius: null,
-        borderBottomRightRadius: null,
-        borderTopLeftRadius: 20, //왼쪽 위 테두리만 둥글게
-        borderTopRightRadius: 20,
-        marginLeft : null,
-        marginTop : null,
-        width: 300,
-        height: 250,
-        source: {uri:'https://ichi.pro/assets/images/max/724/0*Tsd6bDqynxJN1daI'},
-        onPress: () => null,
-    }
-    constructor(props){
-        super(props);
-    }
-    render(){
-      return(
-        <TouchableOpacity
-            onPress={() => this.goNFT_detailScreen()}>
-            <Image style={styles.defaultProps}
-                borderBottomLeftRadius = {this.props.borderBottomLeftRadius}
-                borderBottomRightRadius = {this.props.borderBottomRightRadius}
-                borderTopLeftRadius = {this.props.borderTopLeftRadius}
-                borderTopRightRadius = {this.props.borderTopRightRadius}
-                marginLeft = {this.props.marginLeft}
-                marginTop = {this.props.marginTop}
-                width = {this.props.width}
-                height = {this.props.height}
-                source ={this.props.source}
-            />
-        </TouchableOpacity>
-      )
-    }
-    goNFT_detailScreen() {
-        this.props.navigation.navigate('SUGESST');
-    }
+export default class CardImage extends Component {
+  static defaultProps = {
+    borderBottomLeftRadius: null,
+    borderBottomRightRadius: null,
+    borderTopLeftRadius: 20, //왼쪽 위 테두리만 둥글게
+    borderTopRightRadius: 20,
+    marginLeft: null,
+    marginTop: null,
+    width: 300,
+    height: 250,
+    source: {uri: 'https://ichi.pro/assets/images/max/724/0*Tsd6bDqynxJN1daI'},
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.goNftDetailScreen()}>
+        <Image
+          style={styles.defaultProps}
+          borderBottomLeftRadius={this.props.borderBottomLeftRadius}
+          borderBottomRightRadius={this.props.borderBottomRightRadius}
+          borderTopLeftRadius={this.props.borderTopLeftRadius}
+          borderTopRightRadius={this.props.borderTopRightRadius}
+          marginLeft={this.props.marginLeft}
+          marginTop={this.props.marginTop}
+          width={this.props.width}
+          height={this.props.height}
+          source={this.props.source}
+        />
+      </TouchableOpacity>
+    );
+  }
+  goNftDetailScreen() {
+    this.props.navigation.navigate('SUGESST');
+  }
 }
-    
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({});
--- NFT_exchange/component/common/nftsimpleinfocard/NftSimpleInfoCard.js
+++ NFT_exchange/component/common/nftsimpleinfocard/NftSimpleInfoCard.js
@@ -1,55 +1,47 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import CardImage from '../commonelement/CardImage';
-import NftName from '../commonelement/NftName';
-import Profile from '../commonelement/Profile';
-import NftCost from '../commonelement/NftCost';
+import {View, StyleSheet} from 'react-native';
+import {CardImage, NftName, Profile, NftCost} from '../commonelement';
 import Timer from '../commonelement/Timer';
-import dataList from '../../../datalist.json'
-
-
+import dataList from '../../../datalist.json';
 
 export default class NftSimpleInfoCard extends React.Component {
-  render(){
-    return(
-      <View >
-
-        
+  render() {
+    return (
+      <View>
         {dataList.map((element, index) => (
           <View key={index}>
             <View style={styles.container}>
               <CardImage
-                source={{ uri: element.imageUrl }}
-                navigation={this.props.navigation}/>
-                <View style={styles.cardContainer}>
-                  <View style={styles.informContainer}>
-                    <NftName
-                      title={element.content}
-                      fontSize={20}
-                      navigation={this.props.navigation}/>
-                    <Profile
-                      title={element.title}
-                      marginTop={10}
-                      navigation={this.props.navigation}/>
-                    <NftCost
-                      marginTop={5}
-                      NftCost={'0.01'} />
-                    <Timer 
-                      backgroundColor={'#d3d3d3'}
-                    />
-                  </View>
+                source={{uri: element.imageUrl}}
+                navigation={this.props.navigation}
+              />
+              <View style={styles.cardContainer}>
+                <View style={styles.informContainer}>
+                  <NftName
+                    title={element.content}
+                    fontSize={20}
+                    navigation={this.props.navigation}
+                  />
+                  <Profile
+                    title={element.title}
+                    marginTop={10}
+                    navigation={this.props.navigation}
+                  />
+                  <NftCost marginTop={5} NftCost={'0.01'} />
+                  <Timer backgroundColor={'#d3d3d3'} />
                 </View>
               </View>
             </View>
+          </View>
         ))}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container : {
-    marginTop: 20
+  container: {
+    marginTop: 20,
   },
   cardContainer: {
     backgroundColor: '#d3d3d3',
@@ -58,7 +50,7 @@ const styles = StyleSheet.create({
     width: 300,
     height: 150,
   },
-  informContainer : {
-    marginLeft: 20
+  informContainer: {
+    marginLeft: 20,
   },
-})
+});
