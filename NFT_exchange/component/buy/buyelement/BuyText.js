c4089f3cb4c9b03bed5d04584f98a265627fdff9
minj-L <wonder_minji@naver.com>
2021-07-29 11:02:54 +0900

    코드정리

--- NFT_exchange/component/buy/buyelement/BuyText.js
+++ NFT_exchange/component/buy/buyelement/BuyText.js
@@ -1,53 +1,43 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
 export default class Buy_text extends Component {
+  static defaultProps = {
+    title: 'untitled',
+    marginRight: 20,
+    fontSize: 15,
+    onPress: () => null,
+  };
+  constructor(props) {
+    super(props);
+  }
 
-
-    static defaultProps = {
-        title: 'untitled',
-
-
-        marginRight: 20,
-        fontSize: 15,
-
-        onPress: () => null,
-    }
-    constructor(props) {
-        super(props);
-    }
-
-
-    render() {
-        return (
-
-            <View style={styles.container}>
-                <Text style={styles.sug_txt }>제안가격</Text>
-                <Text style={[
-                    { fontSize: this.props.fontSize },
-                    { marginRight: this.props.marginRight },
-
-                ]}>
-                    {this.props.title}</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.sug_txt}>제안가격</Text>
+        <Text
+          style={[
+            {fontSize: this.props.fontSize},
+            {marginRight: this.props.marginRight},
+          ]}>
+          {this.props.title}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        marginTop: 40, 
-        flexDirection: 'row', 
-        justifyContent: 'space-between' 
-    
-    },
+  container: {
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-    sug_txt: { 
-        marginLeft: 20, 
-        fontSize: 15, 
-        fontWeight: 'bold' 
-    },
-    
-
-}
-)
+  sug_txt: {
+    marginLeft: 20,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
