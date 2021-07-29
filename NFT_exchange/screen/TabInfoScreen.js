d9a11d58cf072a7fc170b24a40cda6b691830cf2
minj-L <wonder_minji@naver.com>
2021-07-29 09:45:54 +0900

    TabInfoScreen 레이아웃 수정

--- NFT_exchange/screen/TabInfoScreen.js
+++ NFT_exchange/screen/TabInfoScreen.js
@@ -1,8 +1,8 @@
 //탭바 정보 탭의 정보 스크린
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import Profile from '../component/common/commonelement/Profile';
-import  I18n  from '../src/config/i18n'
+import I18n from '../src/config/i18n';
 
 export default class Tab_infoScreen extends React.Component {
   render() {
@@ -17,15 +17,15 @@ export default class Tab_infoScreen extends React.Component {
           profile_height={40}
           title={'hyunji'}
           name_marginLeft={20}
-          name_marginTop={8} />
-        <View style={styles.profileUnderLine} />
+          name_marginTop={8}
+        />
+        <View style={styles.profileUpLine} />
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
-
     flex: 1,
     marginTop: 30,
     height: 300,
@@ -42,14 +42,8 @@ const styles = StyleSheet.create({
   },
   profileUpLine: {
     marginTop: 20,
+    marginBottom: 20,
     borderTopWidth: 0.5,
     borderColor: '#DCDCDC',
   },
-  profileUnderLine : { 
-    borderTopWidth: 0.5, 
-    borderColor: '#DCDCDC', 
-  }
-
-
-  
 });
